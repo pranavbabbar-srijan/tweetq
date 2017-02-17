6614732156b374ee62a60e8162e81aa6fd11fd25
joginder <joginderpc@gmail.com>
2017-02-17 23:57:22 +0530

    themed import csv section and header profile

---
+++ web/themes/srijan/js/custom.js
@@ -0,0 +1,7 @@
+//custom js.
+
+(function($) {
+	$(document).ready(function(){
+		$(".-form-tweets-queue-csv-upload .button").insertAfter("#edit-managed-file-upload #edit-managed-file-upload");
+	});
+})(jQuery);
