83ecdf9c0774662dc91e2700242c85bee6e58bac
joginder <joginderpc@gmail.com>
2017-02-20 23:57:53 +0530

    started theming notification section

--- web/themes/srijan/js/custom.js
+++ web/themes/srijan/js/custom.js
@@ -3,5 +3,10 @@
 (function($) {
 	$(document).ready(function(){
 		$(".-form-tweets-queue-csv-upload .button").insertAfter("#edit-managed-file-upload #edit-managed-file-upload");
+		$(".block-tweets-queue .content").append("<a href='#;'></a>");
+		$(".block-tweets-queue .profile + a").click(function() {
+			$(this).toggleClass("active");
+			$(this).siblings(".notifications").toggleClass("active");
+		});
 	});
 })(jQuery);
