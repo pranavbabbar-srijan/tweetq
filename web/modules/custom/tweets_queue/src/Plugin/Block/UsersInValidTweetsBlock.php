ded0c2364e2bb4334a7a070eb55e5923ea799029
chandra shekhar <cshekhar@apigee.com>
2017-02-21 14:10:57 +0530

    added block for invalid tweets

---
+++ web/modules/custom/tweets_queue/src/Plugin/Block/UsersInValidTweetsBlock.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\tweets_queue\Plugin\Block\UsersInValidTweetsBlock.
+ */
+namespace Drupal\tweets_queue\Plugin\Block;
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'twitter' block.
+ *
+ * @Block(
+ *   id = "users_in_valid_tweets_block",
+ *   admin_label = @Translation("Users invalid tweets block"),
+ *   category = @Translation("Twitter users invalid tweets block")
+ * )
+ */
+class UsersInValidTweetsBlock extends BlockBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    global $base_url;
+    $uid = \Drupal::currentUser()->id();
+    
+    $header = array(t('Message'), t('Size'), t('Created'), t('Last Updated'), t('Edit'));
+    
+    $query = \Drupal::database()->select(TWITTER_MESSAGE_QUEUE_TABLE, 'p');
+    $query->fields('p', ['nid', 'message', 'size', 'created' ,'changed']);
+    // Only bring active handlers.
+    $query->condition('p.size', 140, '>');
+    $query->condition('p.uid', $uid);
+
+    $table_sort = $query->extend('Drupal\Core\Database\Query\TableSortExtender')->orderByHeader($header);
+    $pager = $table_sort->extend('Drupal\Core\Database\Query\PagerSelectExtender')->limit(10);
+    $result = $pager->execute();
+
+    $data = array();
+    $total = 0;
+    foreach($result as $row) {
+      $total++;
+      $rows[] = $this->compileData($row);
+    }
+
+    if ($total) {
+      $build = array(
+        '#markup' => ''
+      );
+
+      $build['valid_tweets'] = array(
+        '#theme' => 'table',
+        '#header' => $header,
+        '#rows' => $rows
+      );
+      $build['pager'] = array(
+       '#type' => 'pager'
+      );
+      return $build;
+    }
+    if (!$total) {
+      return array(
+        '#type' => 'markup',
+        '#markup' => t('No invalid tweets found.'),
+      );
+    }
+  }
+
+  private function compileData($row) {
+    $data = array();
+    $data['message'] = $row->message;
+    $data['size'] = $row->size;
+    $data['created'] = $row->created;
+    $data['changed'] = $row->changed;
+    return array('data' => $data);
+  }
+}
