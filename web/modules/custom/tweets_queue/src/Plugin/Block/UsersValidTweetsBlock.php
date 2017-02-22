2c11c6a982108a4b409d18bffbdd900d8a8e2912
chandra shekhar <cshekhar@apigee.com>
2017-02-22 11:39:27 +0530

    modified block added class

--- web/modules/custom/tweets_queue/src/Plugin/Block/UsersValidTweetsBlock.php
+++ web/modules/custom/tweets_queue/src/Plugin/Block/UsersValidTweetsBlock.php
@@ -67,12 +67,16 @@ class UsersValidTweetsBlock extends BlockBase {
   }
 
   private function compileData($row) {
-    $edit_url = Url::fromRoute(TWITTER_TWEET_FORM_ROUTE_NAME, ['nid' => $row->nid,
-      'action' => 'edit']);
-    $delete_url = Url::fromRoute(TWITTER_TWEET_FORM_ROUTE_NAME, ['nid' => $row->nid,
-      'action' => 'delete']);
-    $edit_url_link = \Drupal::l(t('Edit'), $edit_url);
+    $edit_url = Url::fromRoute(TWITTER_TWEET_FORM_ROUTE_NAME,
+      ['nid' => $row->nid, 'action' => 'edit'],
+      ['attributes' => ['class' => 'edit']]
+    );
+    $delete_url = Url::fromRoute(TWITTER_TWEET_FORM_ROUTE_NAME,
+      ['nid' => $row->nid, 'action' => 'delete'],
+      ['attributes' => ['class' => 'delete']]
+    );
 
+    $edit_url_link = \Drupal::l(t("Edit"), $edit_url);
     $delete_url_link = \Drupal::l(t('Delete'), $delete_url);
 
     $data = array();
