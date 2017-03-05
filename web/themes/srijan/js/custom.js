d938334f15c9de16bb1dc1783035efb586356284
joginder <joginderpc@gmail.com>
2017-03-05 23:02:12 +0530

    themed signup form and done some js work to fix text lenght by 140 char.

--- web/themes/srijan/js/custom.js
+++ web/themes/srijan/js/custom.js
@@ -9,6 +9,42 @@
 			$(this).siblings(".notifications").toggleClass("active");
 		});
 	});
+	// $(".block-tweets-queue .item-list + .item-list ul ul li:first-child").each (function () {
+	//   if ($(this).text().length > 140)
+	//     $(this).text($(this).text().substring(0,140) + '...');
+	// });
+    var showChar = 140;  // How many characters are shown by default
+    var moretext = "More >";
+    var lesstext = "Less";
+    
+
+    $('.block-tweets-queue .item-list + .item-list ul ul li:first-child').each(function() {
+        var content = $(this).html();
+ 
+        if(content.length > showChar) {
+ 
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar, content.length - showChar);
+ 
+            var html = c + '<span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+ 
+            $(this).html(html);
+        }
+ 
+    });
+ 
+    $(".morelink").click(function(){
+        if($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
 })(jQuery);
 
 
