e510455e9b939ae2db5b2def4108572d64825de9
hesnvabr <pranav.babbar@srijan.net>
2017-04-10 11:00:17 +0530

    updated settings.local.php

--- web/sites/default/settings.local.php
+++ web/sites/default/settings.local.php
@@ -1,1 +1,17 @@
-../../../.platform/local/shared/settings.local.php
+<?php
+/**
+* @file
+* An example settings.local.php file for Drupal.
+*/
+
+// Add your database configuration here (and uncomment the block).
+
+$databases['default']['default'] = array(
+ 'driver' => 'mysql',
+ 'host' => '127.0.0.1',
+ 'username' => 'root',
+ 'password' => 'root',
+ 'database' => 'tweetqnew',
+ 'prefix' => '',
+);
+$settings['hash_salt'] = 'vasas';
