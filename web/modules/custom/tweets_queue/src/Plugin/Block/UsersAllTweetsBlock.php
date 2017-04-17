230f91587df8dbf8b9d03acda54b705dfd74c313
hesnvabr <pranav.babbar@srijan.net>
2017-04-17 18:37:02 +0530

    Retweeted-value only show values

--- web/modules/custom/tweets_queue/src/Plugin/Block/UsersAllTweetsBlock.php
+++ web/modules/custom/tweets_queue/src/Plugin/Block/UsersAllTweetsBlock.php
@@ -115,7 +115,7 @@ class UsersAllTweetsBlock extends BlockBase {
     $changed = ($row->{TWITTER_FIELD_CHANGED}) ? date(TWITTER_DATE_FORMAT, $row->{TWITTER_FIELD_CHANGED}) : '-';
     $data['changed'] = $changed;
     // $data['changed'] = date(TWITTER_DATE_FORMAT, $row->{TWITTER_FIELD_CHANGED});
-    $data['retweeted'] = t('@times times', array('@times' => $row->tweeted));
+    $data['retweeted'] = t('@times', array('@times' => $row->tweeted));
     $data['edit_link'] = $edit_url_link ;
     $data['delete_link'] = $delete_url_link ;
     return array('data' => $data);
