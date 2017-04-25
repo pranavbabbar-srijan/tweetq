cfd42e2a7439078a316e0cafded0087cefd0b53d
hesnvabr <pranav.bababr@srijan.net>
2017-04-25 17:34:23 +0530

    delete uncommented code of password in registration form

--- web/modules/custom/tweets_queue/src/Form/TwitterSignUpForm.php
+++ web/modules/custom/tweets_queue/src/Form/TwitterSignUpForm.php
@@ -67,15 +67,6 @@ class TwitterSignUpForm extends FormBase {
       ),
     );
 
-    // $form['user_password'] = array(
-    //   '#type' => 'password',
-    //   '#title' => t('Password'),
-    //   '#required' => TRUE,
-    //   '#attributes' => array (
-    //     'placeholder' => t("Required")
-    //   ),
-    // );
-
     $form[SIGNUP_FIELD_WEBSITE] = array(
       '#type' => 'url',
       '#title' => t('Website'),
