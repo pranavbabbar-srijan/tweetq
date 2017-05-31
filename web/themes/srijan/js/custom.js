47bc49da53c1220b01361d9ea7502c9927dfa977
hesnvabr <pranav.bababr@srijan.net>
2017-05-31 18:23:33 +0530

    add fadeOut feature

--- web/themes/srijan/js/custom.js
+++ web/themes/srijan/js/custom.js
@@ -736,7 +736,7 @@
 			if (password.length < 6 && password.length > 0) {
 			$('#result').removeClass()
 			$('#result').addClass('short')
-			$('#result').html('Too Short');
+			$('#result').html('Short').fadeIn(3000).hide(6000);
 			}
 			if (password.length > 7) strength += 1
 			// If password contains both lower and uppercase characters, increase strength value.
