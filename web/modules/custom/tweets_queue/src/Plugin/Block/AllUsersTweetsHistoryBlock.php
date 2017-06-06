f4b7b637fe2f7eecd5911a10065af4e4be45e2e0
chandra shekhar <cshekhar@apigee.com>
2017-06-06 15:04:57 +0530

    added filter on tweets history

--- web/modules/custom/tweets_queue/src/Plugin/Block/AllUsersTweetsHistoryBlock.php
+++ web/modules/custom/tweets_queue/src/Plugin/Block/AllUsersTweetsHistoryBlock.php
@@ -25,8 +25,18 @@ class AllUsersTweetsHistoryBlock extends BlockBase {
     global $base_url;
     $header = array(t('Cron Id'), t('Cron Run Time'), t('Twitter Name'), t('Tweet Message'), t('Tweet Time'), t('Status'));
 
+    $nid = tweets_queue_get_parameter_data(TWITTER_FIELD_NID);
+    $uid = tweets_queue_get_parameter_data(TWITTER_FIELD_UID);
+    $cron_id = tweets_queue_get_parameter_data('cron_id');
+
     $query = \Drupal::database()->select(TWITTER_TWEETS_HISTORY_TABLE, 'p');
     $query->fields('p', [TWITTER_FIELD_NID, TWITTER_FIELD_UID, TWITTER_FIELD_STATUS, TWITTER_FIELD_RETWEETED, TWITTER_FIELD_CODE]);
+    if ($nid) {
+      $query->condition('p.' . TWITTER_FIELD_NID, $nid);
+    }
+    if ($uid) {
+      $query->condition('p.' . TWITTER_FIELD_UID, $uid);
+    }
     $query->addField('p', TWITTER_FIELD_MESSAGE, 'error');
     $query->addField('p', TWITTER_FIELD_CREATED, 'tweet_created_time');
 
@@ -35,10 +45,14 @@ class AllUsersTweetsHistoryBlock extends BlockBase {
     $query->addField('s', 'value', 'screen_name');
 
     $query->leftjoin(TWITTER_CRON_HISTORY_TABLE, 'c', 'p.cron_id=c.id');
+    if ($cron_id) {
+      $query->condition('c.id', $cron_id);
+    }
     $query->fields('c', ['id']);
     $query->addField('c', 'created', 'cron_time');
 
     $query->leftjoin(TWITTER_MESSAGE_QUEUE_TABLE, 'm', 'p.nid = m.nid');
+
     $query->addField('m', TWITTER_FIELD_MESSAGE, TWITTER_FIELD_MESSAGE);
     $query->orderBy('p.created', 'DESC');
 
@@ -55,17 +69,39 @@ class AllUsersTweetsHistoryBlock extends BlockBase {
 
       $message = tweets_queue_decrypt_data($row->{TWITTER_FIELD_MESSAGE});
       $message = tweets_queue_perform_hashtag_highlight($message);
+
+      $message_url = Url::fromRoute(TWITTER_HISTORY_ROUTE_NAME,
+        ['nid' => $row->{TWITTER_FIELD_NID}]
+      );
+      $message_url_link = \Drupal::l($message, $message_url);
+
+      $user_url = Url::fromRoute(TWITTER_HISTORY_ROUTE_NAME,
+        ['uid' => $row->{TWITTER_FIELD_UID}]
+      );
+      $user_url_link = \Drupal::l($row->screen_name, $user_url);
+
+      $cron_url = Url::fromRoute(TWITTER_HISTORY_ROUTE_NAME,
+        ['cron_id' => $row->id]
+      );
+      $cron_url_link = \Drupal::l($row->id, $cron_url);
+
       $error = trim(t($row->error));
       if (empty($error)) {
         $error = ($row->{TWITTER_FIELD_RETWEETED}) ? t('Retweet') : t("Tweet");
       }
 
-      $rows[] = array($row->id, $cron_time, $row->screen_name, $message, $tweet_time, $error);
+      $rows[] = array($cron_url_link, $cron_time, $user_url_link, $message_url_link, $tweet_time, $error);
     }
+
+    $tweet_url = Url::fromRoute(TWITTER_HISTORY_ROUTE_NAME,
+        [],
+        ['attributes' => ['class' => 'tweets-history']]
+    );
+    $tweet_url_link = \Drupal::l(t('Tweets History'), $tweet_url);
 
     if ($total) {
       $build = array(
-        '#markup' => ''
+        '#markup' => $tweet_url_link
       );
 
       $build['all_tweets'] = array(
