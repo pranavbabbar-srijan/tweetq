3baadfefefd84a030d00d425a609818540e8b135
Sumit kumar <sumit.kumar@srijan.net>
2017-07-19 11:10:00 +0530

    tweet msg two time

--- web/themes/srijan/js/custom.js
+++ web/themes/srijan/js/custom.js
@@ -968,8 +968,8 @@
 		});
 
 		  // Fadeout messages.
-		$('.messages, .messages--error').show(100);
-		$('.messages, .messages--error').delay(8000).fadeOut(300);
+		/*$('.messages, .messages--error').show(100);
+		$('.messages, .messages--error').delay(8000).fadeOut(300);*/
 
 		// Message position changes.
 		$(".path-frontpage .region-content .messages--error").insertAfter("#user-login-prefix .form-type-password");
@@ -1058,7 +1058,7 @@
 	    $('#my_tweets > div').animate({ height: 'toggle', opacity: 'toggle' }, '1500');
 	}
 
-	$('.messages').delay(500).animate({'bottom': '0'}, 50);
+	/*$('.messages').delay(500).animate({'bottom': '0'}, 50);*/
 
     // change text of summary 
     $(".user-form summary").text("Settings");
