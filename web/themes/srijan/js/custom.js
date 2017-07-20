44f80def02f0dc2ded7632440ad6aca5eb1fda3f
Sumit kumar <sumit.kumar@srijan.net>
2017-07-20 13:56:01 +0530

    done toaster uniform

--- web/themes/srijan/js/custom.js
+++ web/themes/srijan/js/custom.js
@@ -1056,6 +1056,7 @@
 
 	if ($('#my_tweets > div a').hasClass('active')) {
 	    $('#my_tweets > div').animate({ height: 'toggle', opacity: 'toggle' }, '1500');
+	    &('#my_tweets').addClass('active');
 	}
 
 	
